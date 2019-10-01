fcf434c58ddc372d8d15126e52e6880e912f86ff
Colin Kelleher <43775947+colinkelleher@users.noreply.github.com>
2019-10-01 16:15:36 +0100

    Update Calculator.java

--- Java_Calculator/src/Calculator.java
+++ Java_Calculator/src/Calculator.java
@@ -9,5 +9,23 @@ public class Calculator {
 
         Scanner scannerInput = new Scanner(System.in);
 
-    };
+        System.out.println(("Please input your first number:"));
+        value1 = scannerInput.nextFloat();
+        System.out.println(("Please input your second number:"));
+        value2 = scannerInput.nextFloat();
+        System.out.println(("Please input your operator:"));
+        operator = scannerInput.next().charAt(0);
+
+        switch (operator) {
+            case '+': answer = value1 + value2;
+            break;
+            case '-': answer = value1 - value2;
+            break;
+            case '/': answer = value1 / value2;
+            break;
+            case '*': answer = value1 * value2;
+            break;
+        }
+        System.out.println(answer);
+    }
 }
