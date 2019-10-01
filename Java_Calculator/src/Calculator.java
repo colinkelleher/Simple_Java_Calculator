48246d63533d1975074b795619d110adb92155d5
Colin Kelleher <colink2009@hotmail.com>
2019-10-01 15:58:54 +0100

    Importing Scanner, Initialising value 1, value 2, operator, and answer

---
+++ Java_Calculator/src/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main (String []  args) {
+        float value1;
+        float value2;
+        char operator;
+        double answer = 0;
+
+        Scanner scannerInput = new Scanner(System.in);
+
+    };
+}
